e4770e7a79ba6bc49535bc9e1873da63b0200540
Declan-A-M <dmcdaniel1@tulane.edu>
2026-07-01 21:19:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let lampPost; 
 let Star;
+let Bat
 
 function preload() {
-  lampPost = loadImage("Images/lampPost.png");
-  Star = loadImage("Images/Star.png");
+  lampPost = loadImage("images/lampPost.png");
+  Star = loadImage("images/Star.png");
+  Bat = loadImage("images/Bat.jpg");
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,7 +14,11 @@ function setup() {
 }
 
 function draw() {
-  background(83, 61, 161);
+  if(mouseIsPressed){
+background(0,0,0)
+  }else{background(83,61,161)
+
+  }
   //background(random(125), random(125), random(125))
   rect(0, 500, 100, 600);
   rect(285, 500, 100, 400)
@@ -23,12 +29,16 @@ function draw() {
  fill(29, 31, 29)
    rect(400, 500, 100, 500)
   circle(500, 200, 100)
-  {circle(400,100,100)
-  }
+  circle(400,100,100)
+  
 line (400, 100, 300, 50)
   line (370, 150, 260, 100)
   line (580, 200, 370, 25)
 
   image(lampPost, 200, 400)
-  image(Star, 350, 450)
+  image(Star, 1000, 200)
+  image(Bat, 700, 50)
+
+  
+
 }
